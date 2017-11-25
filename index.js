65e69dfd6779e7cd873ef029bfc0caf9b50a1966
Kyle Bonar <kylewb2011@gmail.com>
2017-11-25 15:53:12 -0600

    Application asks for path to directory, scans files, outputs number of files, and begins comparison algorithm.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var Jimp = require("jimp");
-var path = require("path");
+const Jimp = require("jimp");
+const path = require("path");
 const readline = require("readline");
+const promisify = require("promisify-node");
+const fs = promisify("fs");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,42 +10,75 @@ const rl = readline.createInterface({
 });
 
 rl.question("path to directory?", answer => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+  if (answer.length === 0) {
+    console.log("You must enter a directory!");
+  }
+
+  //IFEE function to use async/await
+  (async function(answer) {
+    console.log("starting timer");
+    console.time("timer");
+    //get directory
+    const dir = path.resolve(__dirname, answer);
+    try {
+      //get all files in directory
+      let files = await fs.readdir(dir);
+      files = files.filter(file => {
+        return (
+          file.substr(file.length - 3) === "png" ||
+          file.substr(file.length - 3) === "PNG" ||
+          file.substr(file.length - 3) === "jpg" ||
+          file.substr(file.length - 3) === "JPG"
+        );
+      });
+
+      console.log(`${files.length} files to scan through!`);
+
+      const dict = new Set();
+      for (let i = 0; i < files.length; i++) {
+        if (!dict.has(files[i])) {
+          dict[files[i]] = await Jimp.read(`${dir}/${files[i]}`);
+        }
+        for (let j = i + 1; j < files.length; j++) {
+          if (!dict.has(files[j])) {
+            dict[files[j]] = await Jimp.read(`${dir}/${files[j]}`);
+          }
+
+          const result = Jimp.distance(dict[files[i]], dict[files[j]]);
+          console.log(
+            `Percent similar between ${files[i]} and ${files[j]} is %${(1 -
+              result) *
+              100}`
+          );
+
+          if (result < 0.05) {
+            try {
+              await fs.rename(
+                `${dir}/${files[i]}`,
+                `${dir}/Duplicates/${files[i]}`
+              );
+              await fs.rename(
+                `${dir}/${files[j]}`,
+                `${dir}/Duplicates/${files[j]}`
+              );
+
+              console.log(`moved ${files[i]} and ${files[j]}`);
+            } catch (err) {
+              console.log(err);
+            }
+            files.splice(j, 1);
+            break;
+          }
+        }
+        //console.log(files);
+        // console.log(i);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.timeEnd("timer");
+  })(answer);
 
   rl.close();
 });
-
-// Jimp.read("IMG_0028.PNG", function (err, image) {
-//     if (err) throw err;
-//     console.log(image.getPixelColor(200,200));
-// });
-
-var img1 = "sample_images/1.png";
-var img2 = "sample_images/2.png";
-var img3 = "sample_images/3.png";
-(async function() {
-  var j = await Jimp.read(img1);
-  var m = await Jimp.read(img2);
-  var p = await Jimp.read(img3);
-
-  var k = Jimp.diff(j, m);
-  console.log(k);
-})();
-// Jimp.read(img1)
-//   .then(function(lenna) {
-//     console.log(lenna);
-//     return Jimp.read(img2);
-//   })
-//   .then(function(res) {
-//     console.log(res.);
-//   })
-//   .catch(function(err) {
-//     console.error(err);
-//   });
-
-// var j = Jimp.distance(Jimp.read(img1), Jimp.read(img2));
-
-// console.log(j);
-// console.log(Jimp.distance(img2, img3))
-// console.log(Jimp.distance(img1, img3))
